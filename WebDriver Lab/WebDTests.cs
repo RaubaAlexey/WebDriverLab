d381a5ae820321a5e36fbb071126a174c9177193
RaubaAlexey <www.rauba@mail.ru>
2017-10-23 16:53:28 +0300

    commit #1 lab4

---
+++ WebDriver Lab/WebDTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace WebDriver_Lab
+{
+    [TestFixture]
+    public class WebDTests
+    {
+        [Test]
+        public void openGoogle()
+        {
+            IWebDriver firefox = new FirefoxDriver();
+            firefox.Navigate().GoToUrl("http://www.github.com");
+
+            IWebElement buttonSearch = firefox.FindElement(By.XPath("//a[@class='text-bold text-white no-underline']"));
+            buttonSearch.Click();
+
+
+            IWebElement loginWrite = firefox.FindElement(By.Id("login_field"));
+            loginWrite.SendKeys("RaubaAlexey");
+
+            IWebElement passWrite = firefox.FindElement(By.Id("password"));
+            passWrite.SendKeys("RaubaL58824");
+
+            IWebElement loginClick = firefox.FindElement(By.Name("commit"));
+            loginClick.Click();
+
+        }
+
+    }
+}
